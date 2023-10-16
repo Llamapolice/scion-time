75f1f3e50a54237b5d0d1f4a3e0c1e552cda18e0
Luca Faga <lufaga@student.ethz.ch>
2023-10-16 22:20:43 +0200

    Barebones simulation clock implementation

---
+++ simulation/simclk.go
@@ -0,0 +1,43 @@
+package simulation
+
+import (
+	"time"
+
+	"example.com/scion-time/base/timebase"
+)
+
+type SimulationClock struct {
+	// TODO: some logging function here
+}
+
+func (c *SimulationClock) Epoch() uint64 {
+	//TODO implement me
+	panic("SimulationClock.Epoch() implement me")
+}
+
+func (c *SimulationClock) Now() time.Time {
+	//TODO implement me
+	panic("SimulationClock.Now(): implement me")
+}
+
+func (c *SimulationClock) MaxDrift(duration time.Duration) time.Duration {
+	//TODO implement me
+	panic("SimulationClock.MaxDrift(): implement me")
+}
+
+func (c *SimulationClock) Step(offset time.Duration) {
+	//TODO implement me
+	panic("SimulationClock.Step(): implement me")
+}
+
+func (c *SimulationClock) Adjust(offset, duration time.Duration, frequency float64) {
+	//TODO implement me
+	panic("SimulationClock.Adjust(): implement me")
+}
+
+func (c *SimulationClock) Sleep(duration time.Duration) {
+	//TODO implement me
+	panic("SimulationClock.Sleep(): implement me")
+}
+
+var _ timebase.LocalClock = (*SimulationClock)(nil)
