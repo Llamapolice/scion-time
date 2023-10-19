0ff8208a7d8dfaf44e0c04d98e58531ff5a02a8f
Luca Faga <lufaga@student.ethz.ch>
2023-10-19 22:40:33 +0200

    Small update on netbase, but put on ice for now

--- base/netbase/netprovider.go
+++ base/netbase/netprovider.go
@@ -1,6 +1,7 @@
 package netbase
 
 import (
+	"example.com/scion-time/net/udp"
 	"net"
 	"net/netip"
 	"time"
@@ -13,4 +14,9 @@ type ConnProvider interface {
 	WriteToUDPAddrPort(b []byte, addr netip.AddrPort) (int, error)
 	SetDeadline(t time.Time) error
 	LocalAddr() net.Addr // based on core/client/client_scion.go:138, maybe change it to straight up give out the port?
+
+	ListenUDP(network string, laddr *udp.UDPAddr) (*ConnProvider, error)
+	EnableTimestamping(n ConnProvider, localHostIface string) error
+	SetDSCP(n ConnProvider, dscp uint8) error
+	ReadTXTimestamp(n ConnProvider) (time.Time, uint32, error)
 }
